5b121a3561c2dc8b77c4bc4d236cb28c86271b38
JVvix <fayedotly@gmail.com>
2020-07-17 14:30:26 -0700

    fixed bug and added checkbox feature

--- tkToDo.py
+++ tkToDo.py
@@ -3,42 +3,43 @@ import random
 import os
 import json
 
-location = 3
+location = 5
 
-def makeNew(name):
-    global location
-    name2 = name
-    name = exec("%s = %d" % (name, 2))
-    name = Checkbutton(root, text=name2).grid(row=location, column=0)
-
-def update_screen():
-    global location
-    for i in range(len(tasks)):
-        if tasks[i] != "":
-            location += 1
-            makeNew(tasks[i])
-
-def add_task():
-    global tasks, location
-    tasks.append(newTask.get())
-    update_screen()
-
-# def delete_all():
-#     global tasks, location
-    
-#     location = 3
-
-# def delete_task():
-#     tasks = tasks[:-1]
-#     update_screen()
+def save_and_quit():
+    f = open("save_data.txt", "w")
+    f.write(str(tasks))
+    f.close()
+    root.destroy()
 
 def save():
     f = open("save_data.txt", "w")
-    f.write(tasks)
-    root.destroy()
+    f.write(str(tasks))
+    f.close()
 
-# ### program starts ###
+def add_task():
+    global location
+    taskName = newTask.get()
+    tasks.append(taskName)
+    word = Checkbutton(task_frame, text=str(taskName)).grid(row=location, column=0)
+    location += 1
 
+def add_saved_task(taskName):
+    global location
+    word = Checkbutton(task_frame, text=taskName).grid(row=location, column=0)
+    location += 1
+
+def delete_all():
+    global tasks
+    tasks = []
+    task_frame.grid_forget()
+
+### program starts ### 
+root = Tk()
+root.title("My To-Do List")
+root.geometry("260x220")
+
+task_frame = Frame(root)
+task_frame.grid(row=4)
 f = open("save_data.txt", "r")
 text = f.read()
 
@@ -47,14 +48,11 @@ if len(text) == 0:
 else:
     tasks = text.strip('][').split(", ")
     for i in range(len(tasks)):
-        tasks[i] = tasks[i].replace("'", "")
+        tasks[i] = tasks[i].replace("'", "") 
     f.close()
 
-print(tasks)
-
-root = Tk()
-root.title("My To-Do List")
-root.geometry("260x220")
+for i in range(len(tasks)):
+    add_saved_task(tasks[i])
 
 lbl_title = Label(root, text="To Do List").grid(row=1, column=0)
 
@@ -64,10 +62,9 @@ newTask = Entry(root)
 newTask.grid(row=3, column=0)
 
 add_button = Button(root, text="Add task", command=add_task).grid(row=3, column=5)
-# delete_all_button = Button(root, text="Delete all tasks", command=delete_all).grid(row=4, column=5)
-# delete_button = Button(root, text="Delete last task", command=delete_task).grid(row=5, column=5)
-# exit_button = Button(root, text="Exit", command=save).grid(row=6, column=5)
+delete_all_button = Button(root, text="Delete all tasks", command=delete_all).grid(row=4, column=5)
+save_button = Button(root, text="Save", command=save).grid(row=5, column=5)
+save_and_quit_button = Button(root, text="Save and Exit", command=save_and_quit).grid(row=6, column=5)
 
-root.protocol("WM_DELETE_WINDOW", save)
-# root.after(2, update_screen)
+root.protocol("WM_DELETE_WINDOW", save_and_quit)
 root.mainloop()
